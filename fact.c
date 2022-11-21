079d8640c17e4d9564747f40f70017daea71014f
Akshataagam <118714274+Akshataagam@users.noreply.github.com>
2022-11-21 12:44:33 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+void add()
+{
+  int a=10,b=20;
+  printf("the addition is=%d",a+b);
+} 
 int main()
 {
   int fact=1,i,num;
@@ -9,5 +14,6 @@ int main()
     fact=fact*num;
   }
   printf("the factorial=%d",fact);
+  add();
   return 0;
 }
